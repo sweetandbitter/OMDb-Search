3c7a87f1a14b1650d6f46f8b97de12f08e31f40e
Di <yaodi891024@gmail.com>
2017-04-03 01:11:59 -0500

    add movie detail pages , add css and logic.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 angular.module("myApp",[])
 	.controller("searchMovie",["$scope", "$http", function($scope, $http){
+		$scope.showDetails = true;
 		$scope.getMovie = function(){
 			$http.get("http://www.omdbapi.com/?s=" + $scope.movieName + "&type=movie&r=json")
 			.success(function(data, status){
-				$scope.details = data.Search;
+				$scope.movies = data.Search;
 				$scope.totalNum = data.totalResults;
 				$scope.response = data.Response;
-				angular.forEach($scope.details, function(value, key){
+				/*angular.forEach($scope.details, function(value, key){
 					console.log(value);
-				})
+				})*/
+				$scope.showDetails = true;
 			})
 			.error(function(data, status){
 				console.log("Not Found");
 			})
+		}
+		$scope.showMovieDetails = function(movie){
+			$http.get('https://www.omdbapi.com/?i=' + movie.imdbID + '&plot=full&r=json&tomatoes=true')
+				.success(function(data, statuse){
+					$scope.moviedetail = data;
+					$scope.showDetails = !$scope.showDetails;
+					console.log($scope.moviedetail);
+				})
 		}
 	}])
 	.directive("movieSearchResults", function(){
