89b9dbf9ee6aab556de76df549b83b340c01cf2f
Di <yaodi891024@gmail.com>
2017-04-02 22:30:40 -0500

    add show movie top 10 results.

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@ angular.module("myApp",[])
 		$scope.getMovie = function(){
 			$http.get("http://www.omdbapi.com/?s=" + $scope.movieName + "&type=movie&r=json")
 			.success(function(data, status){
-				var details = data.Search;
-				var totalNum = data.totalResults;
-				var response = data.Response;
-				angular.forEach(details, function(value, key){
-					
+				$scope.details = data.Search;
+				$scope.totalNum = data.totalResults;
+				$scope.response = data.Response;
+				angular.forEach($scope.details, function(value, key){
+					console.log(value);
 				})
 			})
 			.error(function(data, status){
 				console.log("Not Found");
 			})
 		}
-		
 	}])
+	.directive("movieSearchResults", function(){
+		return {
+			restrict: "EA",
+			templateUrl: "template/showResults.html"
+		};
+	});
