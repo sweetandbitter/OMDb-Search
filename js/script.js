6747ee5e52678d69a03b8bbf88150e0f1527453a
Di <yaodi891024@gmail.com>
2017-04-02 21:34:47 -0500

    call OMDB API get data and add css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 angular.module("myApp",[])
 	.controller("searchMovie",["$scope", "$http", function($scope, $http){
-		$http.get("http://www.omdbapi.com/?s=ghost")
+		$scope.getMovie = function(){
+			$http.get("http://www.omdbapi.com/?s=" + $scope.movieName + "&type=movie&r=json")
 			.success(function(data, status){
-				console.log(data);
+				var details = data.Search;
+				var totalNum = data.totalResults;
+				var response = data.Response;
+				angular.forEach(details, function(value, key){
+					
+				})
 			})
+			.error(function(data, status){
+				console.log("Not Found");
+			})
+		}
+		
 	}])
